6dc6869627ff7a9ae659ed75eb29c0aa46990a8a
Jean Carlos <jcpds14@gmail.com>
2023-05-23 19:10:33 -0300

    add files loops and arrays

---
+++ loops/src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+        // para uma variável que inica em 1, e vai até 10, mudando 1 por 1, faça:
+        //Tabuada dos 9
+        for (int i = 0; i <= 10; i++) {
+            int j = 9;
+                System.out.println(j + "x" + i + " = " + j * i);
+
+        }
+    }
+}
