9389153fc45c224fd2d97dfad63ff90b9ab75a8e
Jean Carlos <jcpds14@gmail.com>
2023-05-24 21:45:31 -0300

    learning about constructors, methods, encapsulation

--- cursoIntroPOOAda/src/Animals/Dog.java
+++ cursoIntroPOOAda/src/Animals/Dog.java
@@ -1,30 +1,106 @@
 package Animals;
 
 public class Dog {
+//Attributes
+    private String name;
+    private String color;
+    private double height;
+    private double weight;
+    private int age;
+    private String stateOfMind;
 
-    public String name;
-    public String cor;
-    public double height;
-    public double weight;
-    public int age;
+    //Default constructor
+    public Dog(){}
 
-    public String stateOfMind;
+    //Aditionals constructors
+    public Dog(String name, String color, double height, double weight, int age, String stateOfMind) {
+        this.name = name;
+        this.color = color;
+        this.height = height;
+        this.weight = weight;
+        this.age = age;
+        this.stateOfMind = stateOfMind;
+    }
 
-    public void eat(){}//Method - in this case, is public, don't use return value, don't have parameters
-    public void bark(){
+    //Methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getStateOfMind() {
+        return stateOfMind;
+    }
+
+    public void eat() {
+    }//Method - in this case, is public, don't use return value, don't have parameters
+
+    public void bark() {
         System.out.println("AU AU");
     }
-    public String take(){
+
+    public String take() {
         return "Bolinha";
     }
-    public String interact(String action){
-        if (action.equals("carinho")){
-            this.stateOfMind = "Feliz";
-        }else if (action.equals("vai dormir!")){
-            this.stateOfMind = "Bravo";
-        }else{
-            this.stateOfMind = "Neutro";
+
+    public String interact(String action) {
+
+        switch (action) {
+            case "carinho":
+                this.stateOfMind = "Feliz";
+                break;
+            case "vai dormir!":
+                this.stateOfMind = "Bravo";
+                break;
+            case "Pisar na patinha":
+                this.stateOfMind = "Triste";
+            default:
+                this.stateOfMind = "Neutro";
         }
+
+
+//        if (action.equals("carinho")){
+//            this.stateOfMind = "Feliz";
+//        }else if (action.equals("vai dormir!")){
+//            this.stateOfMind = "Bravo";
+//        }else{
+//            this.stateOfMind = "Neutro";
+//        }
         return stateOfMind;
     }
 
--- cursoIntroPOOAda/src/Main.java
+++ cursoIntroPOOAda/src/Main.java
@@ -4,19 +4,20 @@ public class Main {
     public static void main(String[] args) {
 
         Dog dogOne = new Dog();
+        Dog dogTwo = new Dog("Aron", "Marrom", 50, 20, 1, "nada");
 
-        dogOne.name = "Jimmy";
-        dogOne.age = 9;
-        dogOne.cor = "Marrom";
-        dogOne.height = 50;
-        dogOne.weight = 25;
+        dogOne.setName("Jimmy");
+        dogOne.setAge(9);
+        dogOne.setColor("Marrom");
+        dogOne.setHeight(50);
+        dogOne.setWeight(25);
 
-        //dogOne.bark();
-        //System.out.println(dogOne.name + " pegou uma " + dogOne.take());
+        System.out.println(dogTwo.getName());
+        System.out.println(dogTwo.getAge());
 
-        System.out.println(dogOne.name + " está " + dogOne.interact("carinho"));
-        System.out.println(dogOne.name + " está " + dogOne.interact("nada"));
-        System.out.println(dogOne.name + " está " + dogOne.interact("vai dormir!"));
+        System.out.println(dogOne.getName() + " está " + dogOne.interact("carinho"));
+        System.out.println(dogOne.getName() + " está " + dogOne.interact("nada"));
+        System.out.println(dogOne.getName() + " está " + dogOne.interact("vai dormir!"));
 
     }
 }
